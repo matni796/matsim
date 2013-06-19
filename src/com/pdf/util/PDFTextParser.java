9617df6594de2567b188cc711f447fbb53800b1a
Simon Carmelid <carmelid@gmail.com>
2013-06-19 21:19:33 +0200

    fixar
    och trixar

---
+++ src/com/pdf/util/PDFTextParser.java
@@ -0,0 +1,16 @@
+package com.pdf.util;
+
+import org.apache.pdfbox.pdfparser.PDFParser;
+
+public class PDFTextParser {
+	private PDFParser parser=null;
+	
+	public PDFTextParser(String string) {
+		
+	}
+
+	public static void main(String args[]){
+		PDFTextParser pdf=new PDFTextParser("Location");
+	}
+
+}
